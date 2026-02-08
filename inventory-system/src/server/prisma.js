79375c9d235141ab5e36658bc06e074a47fedd21
root <root@DESKTOP-OCQA83D.localdomain>
2026-02-08 17:43:58 +0800

    test1

--- inventory-system/src/server/prisma.js
+++ inventory-system/src/server/prisma.js
@@ -1,6 +1,5 @@
-// 直接从生成的客户端文件中导入PrismaClient，绕过@prisma/client包
-// 这是一个临时解决方案，因为@prisma/client/default.js试图导入不存在的模块
-const { PrismaClient } = require('../../node_modules/.prisma/client/client');
+// 从@prisma/client导入PrismaClient
+const { PrismaClient } = require('@prisma/client');
 
 // 创建并导出PrismaClient实例
 const prisma = new PrismaClient();
